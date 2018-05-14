a475b9cbd93d3c260f7420e2e2ac462c5005c755
Maksym Akulov <maksakulov@hotmail.com>
2018-05-14 10:39:10 +0300

    Add functionality to parking class library(fixes #3)

---
+++ ParkingLibrary/IParkingPlace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLibrary
+{
+    public interface IParkingPlace
+    {
+        int AddCar(Car car);
+        int RemoveCar(int car_id);
+        void RefillCarBalance(int car_id, int sum_to_refill);
+        void WriteOff(object obj);
+        List<Transaction> GetTransactionsByLastMinute();
+        int GetFreeSpaceOnParking();
+        void SaveTransactionToFile(object obj);
+        List<string> GetTransactionsFromFile();
+        void StartDay();
+        int GetBusySpaceOnParking();
+        int TotalParkingProfit();
+        int ParkingProfitByLastMinute();
+    }
+}
